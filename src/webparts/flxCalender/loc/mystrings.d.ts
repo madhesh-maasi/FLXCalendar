5a06d42ce0d7d41f6e3830d80fe40a84b3d1750a
madhesh-maasi <madheshmaasi3@gmail.com>
2021-05-13 10:03:58 +0530

    FirstPush

---
+++ src/webparts/flxCalender/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFlxCalenderWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FlxCalenderWebPartStrings' {
+  const strings: IFlxCalenderWebPartStrings;
+  export = strings;
+}
